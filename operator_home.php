09acfb49c79397a4f3e057a1904b640c68b1e0c4
Raie01 <88224803+Raie01@users.noreply.github.com>
2021-11-06 15:47:57 +0800

    tweaked operator home and login

--- operator_home.php
+++ operator_home.php
@@ -13,46 +13,129 @@ if(empty($_SESSION['username']) && empty($_SESSION['password'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Document</title>
 </head>
 <body>
     <a href="logout.php">Logout</a>
     <?php 
     $user = $_SESSION['username'];
+    $dept = $_SESSION['dept_id'];
     echo 'Hello ' . $user; 
+
     ?>
+<div class="container d-flex flex-column">
+
 <?php
-$get_queue = "SELECT queuenumber.id, queuenumber.purpose_id, queuenumber.remarks,
+$get_queue = "SELECT queuenumber.id, queuenumber.purpose_id, queuenumber.remarks, queuenumber.queue_status,
 purposes.purpose_name, purposes.dept_id
 FROM queuenumber 
-LEFT JOIN purposes ON purposes.id = queuenumber.purpose_id";
+LEFT JOIN purposes ON purposes.id = queuenumber.purpose_id 
+WHERE dept_id = $dept AND queuenumber.queue_status = 'Queuing'
+LIMIT 1";
 $get_num = mysqli_query($conn, $get_queue); ?>
 
+<div class="card" style="display: grid; place-items:center;">
+    <div class="card-body">
+    <?php if(mysqli_num_rows($get_num)>0) {
+    while($row = mysqli_fetch_array($get_num)){ ?>
+    <form action="" method="POST">
+        <?php echo 'Current Number : ' . $row['id'] ?>
+        <input type="hidden" value="<?php echo $row['id'] ?>" name="id">
+        <input type="hidden" value="Done" name="statusUp">
+        <input type="submit" name="Next" value="Next">
+    </form>
+    </div>
+    <?php
+    }
+    }else{
+        echo "No Queued yet";
+    }
+?>   
+</div>
+</div>
+
+<?php
+    $get_queue = "SELECT queuenumber.id, queuenumber.purpose_id, queuenumber.remarks, queuenumber.queue_status,
+    purposes.purpose_name, purposes.dept_id
+    FROM queuenumber 
+    LEFT JOIN purposes ON purposes.id = queuenumber.purpose_id 
+    WHERE dept_id = $dept AND queuenumber.queue_status = 'Queuing'";
+    $get_num = mysqli_query($conn, $get_queue); ?>
 <table class="table">
-    <thead>
-    <tr>
-    <th>Purpose</th>
-    <th>Remarks</th>
-    <th> Actions </th>
-    
-    </tr>
+    <thead class="bg-dark text-white">
+        <tr>
+            <th>ID</th>
+            <th>Purpose</th>
+            <th>Remarks</th>
+            <th> Actions </th>
+        </tr>
     </thead>
-<?php
-    if(mysqli_num_rows($get_num)>0) {
-    while($row = mysqli_fetch_array($get_num)){
-?>
-<tbody>
-<tr>
-    <td><?php echo $row['purpose_name']?> </td>
-    <td><?php echo $row['remarks']?> </td>
-    <td><a href="operator_home.php?id=<?php $row['id']?>">Next</a></td>
-</tr>
-</tbody>  
-<?php
+<?php if(mysqli_num_rows($get_num)>0) {
+    while($row = mysqli_fetch_array($get_num)){ ?>
+    <tbody>
+        <tr>
+            <td><?php echo $row['id']?></td>
+            <td><?php echo $row['purpose_name']?></td>
+            <td><?php echo $row['remarks']?> </td>
+            <td><a href="operator_home.php?Next=<?php $row['id']?>">Next</a></td>
+        </tr>
+    </tbody>  
+    <?php
+        }
     }
-    }
-?>    
+    ?>    
 </table>
+
+
+<p class="display-4">LAST CALLED NUMBER</p>
+<?php
+    $get_queue = "SELECT queuenumber.id, queuenumber.purpose_id, queuenumber.remarks, queuenumber.queue_status,
+    purposes.purpose_name, purposes.dept_id
+    FROM queuenumber 
+    LEFT JOIN purposes ON purposes.id = queuenumber.purpose_id 
+    WHERE dept_id = $dept AND queuenumber.queue_status = 'Done'";
+    $get_num = mysqli_query($conn, $get_queue); ?>
+<table class="table">
+    <thead class="bg-dark text-white">
+        <tr>
+            <th>ID</th>
+            <th>Purpose</th>
+            <th>Remarks</th>
+        </tr>
+    </thead>
+<?php if(mysqli_num_rows($get_num)>0) {
+    while($row = mysqli_fetch_array($get_num)){ ?>
+    <tbody>
+        <tr>
+            <td><?php echo $row['id']?></td>
+            <td><?php echo $row['purpose_name']?></td>
+            <td><?php echo $row['remarks']?> </td>
+        </tr>
+    </tbody>  
+    <?php
+        }
+    }
+    ?>    
+</table>
+
 </body>
 </html>
+
+<?php 
+    if(isset($_POST['Next'])){
+        $id = $_POST['id'];
+        $statusUp = $_POST['statusUp'];
+
+        $update =  "UPDATE queuenumber SET queue_status = '$statusUp' WHERE id = $id";
+            $query = mysqli_query($conn, $update);
+            
+            if($query){
+                header("location: operator_home.php"); 
+                exit();
+            }else{
+                echo "Error" .$conn->error;
+            }
+        } 
+?>
 
--- operator_login.php
+++ operator_login.php
@@ -2,29 +2,34 @@
 include "connection.php";
 session_start();
 
-if (isset($_SESSION['username']) && isset($_SESSION['password'])){
+if (isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESSION['dept_id'])){
   header("location:index.php");
 }
   if(isset($_POST['login'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $dept = $_POST['dept_id'];
     
     if(empty($_POST['username']) && empty($_POST['password'])){
-      $error = "please input username & password";
+      echo "please input username & password";
     }
     else if(empty($_POST['username'])){
-      $error = "please input username";
+      echo "please input username";
 
     }
     else if(empty($_POST['password'])){
-      $error = "please input password";
-    }else{
-      $sql_users= "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+      echo "please input password";
+    }
+    else if(empty($_POST['dept_id'])){
+      echo "Tell your department";
+    } else{
+      $sql_users= "SELECT * FROM user WHERE username = '$username' AND password = '$password' AND dept_id = $dept";
       $query_users = mysqli_query($conn, $sql_users);
       $result = mysqli_num_rows($query_users);
       if($result == 1){
         $_SESSION['username'] = $username;
         $_SESSION['password'] = $password;
+        $_SESSION['dept_id'] = $dept;
         header("location:operator_home.php");
         exit();
       }else{
@@ -49,6 +54,21 @@ if (isset($_SESSION['username']) && isset($_SESSION['password'])){
         <input type="text" name="username">
         <label for="">Password</label>
         <input type="password" name="password">
+        <?php 
+        $sql = "SELECT * FROM department";
+        $query = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($query)>0) { ?>
+        <select name="dept_id">
+            <option value="">Select departments</option>
+        <?php 
+        while($row = mysqli_fetch_array($query)){
+            $dept_id = $row['id'];
+            $dept_name = $row['departments'];
+            echo "<option value = '$dept_id'>$dept_name</option>"
+            ?> 
+        <?php } ?>
+        </select>
+        <?php }?> 
         <input type="submit" name="login">
     </form>
 </body>
