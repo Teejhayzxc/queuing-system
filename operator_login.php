bff2e5d1b0c0f4c5b96edeffb743edb3f8b9b0f5
Raie01 <88224803+Raie01@users.noreply.github.com>
2021-11-21 02:09:01 +0800

    login update
    nagtry ako gamitin yung last name ng operator instead of username

--- operator_login.php
+++ operator_login.php
@@ -2,7 +2,7 @@
 include "connection.php";
 session_start();
 
-if (isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESSION['dept_id'])){
+if (!isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESSION['dept_id'])){
   header("location:index.php");
 }
   if(isset($_POST['login'])){
@@ -15,7 +15,6 @@ if (isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESS
     }
     else if(empty($_POST['username'])){
       echo "please input username";
-
     }
     else if(empty($_POST['password'])){
       echo "please input password";
@@ -23,7 +22,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESS
     else if(empty($_POST['dept_id'])){
       echo "Tell your department";
     } else{
-      $sql_users= "SELECT * FROM user WHERE username = '$username' AND password = '$password' AND dept_id = $dept";
+      $sql_users= "SELECT * FROM user WHERE last_name = '$username' AND password = '$password' AND dept_id = $dept";
       $query_users = mysqli_query($conn, $sql_users);
       $result = mysqli_num_rows($query_users);
       if($result == 1){
@@ -37,41 +36,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESS
       }
     }
   }
-
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="" method="POST">
-        <label for="">Username</label>
-        <input type="text" name="username">
-        <label for="">Password</label>
-        <input type="password" name="password">
-        <?php 
-        $sql = "SELECT * FROM department";
-        $query = mysqli_query($conn, $sql);
-        if(mysqli_num_rows($query)>0) { ?>
-        <select name="dept_id">
-            <option value="">Select departments</option>
-        <?php 
-        while($row = mysqli_fetch_array($query)){
-            $dept_id = $row['id'];
-            $dept_name = $row['departments'];
-            echo "<option value = '$dept_id'>$dept_name</option>"
-            ?> 
-        <?php } ?>
-        </select>
-        <?php }?> 
-        <input type="submit" name="login">
-    </form>
-</body>
-</html>
 
 
