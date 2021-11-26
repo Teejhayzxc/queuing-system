7b1db9ecef59c27fff93c3ca1fd1375d6a7c14da
Raie01 <88224803+Raie01@users.noreply.github.com>
2021-11-27 03:49:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ if(!empty($_SESSION['username']) && empty($_SESSION['password'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/styles.css">
+    <script>window.history.forward();</script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <title>Document</title>
 </head>
